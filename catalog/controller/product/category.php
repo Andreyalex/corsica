11314d4390bccb56a476ac22ad745ff9d0668b54
Andrey <dev.andreyalek@gmail.com>
2015-06-30 00:57:51 +0300

    Added options to products

--- catalog/controller/product/category.php
+++ catalog/controller/product/category.php
@@ -222,7 +222,8 @@ class ControllerProductCategory extends Controller {
 						'tax'         => $tax,
 						'minimum'     => $result['minimum'] > 0 ? $result['minimum'] : 1,
 						'rating'      => $result['rating'],
-						'href'        => $this->url->link('product/product', 'path=' . $this->request->get['path'] . '&product_id=' . $result['product_id'] . $url)
+						'href'        => $this->url->link('product/product', 'path=' . $this->request->get['path'] . '&product_id=' . $result['product_id'] . $url),
+                        'options'     => $result['options']
 					);
 				}
 
