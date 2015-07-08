49ced4430d8effc78bf2588c90cc40d09cf7e3d5
root <root@yomayka.com>
2015-07-08 18:52:41 +0100

    Added old collections, fixed callback, fixed translates

---
+++ catalog/controller/oldcollection/category.php
@@ -0,0 +1,85 @@
+<?php
+class ControllerOldCollectionCategory extends Controller {
+	public function index() {
+		$data = array();
+        $collection = true;
+
+        if ($collection) {
+        	$data['heading_title'] = $this->language->get('text_error');
+
+        	$data['breadcrumbs'][] = array(
+	            'text' => $this->language->get('text_home'),
+	            'href' => $this->url->link('common/home')
+	        );
+
+        	$data['column_left'] = $this->load->controller('common/column_left');
+            $data['column_right'] = $this->load->controller('common/column_right');
+            $data['content_top'] = $this->load->controller('common/content_top');
+            $data['content_bottom'] = $this->load->controller('common/content_bottom');
+            $data['footer'] = $this->load->controller('common/footer');
+            $data['header'] = $this->load->controller('common/header');
+
+            if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/oldcollection/collection.tpl')) {
+                $this->response->setOutput($this->load->view($this->config->get('config_template') . '/template/oldcollection/collection.tpl', $data));
+            } else {
+                $this->response->setOutput($this->load->view('default/template/oldcollection/collection.tpl', $data));
+            }
+        } else {
+            $url = '';
+
+            if (isset($this->request->get['path'])) {
+                $url .= '&path=' . $this->request->get['path'];
+            }
+
+            if (isset($this->request->get['filter'])) {
+                $url .= '&filter=' . $this->request->get['filter'];
+            }
+
+            if (isset($this->request->get['sort'])) {
+                $url .= '&sort=' . $this->request->get['sort'];
+            }
+
+            if (isset($this->request->get['order'])) {
+                $url .= '&order=' . $this->request->get['order'];
+            }
+
+            if (isset($this->request->get['page'])) {
+                $url .= '&page=' . $this->request->get['page'];
+            }
+
+            if (isset($this->request->get['limit'])) {
+                $url .= '&limit=' . $this->request->get['limit'];
+            }
+
+            $data['breadcrumbs'][] = array(
+                'text' => $this->language->get('text_error'),
+                'href' => $this->url->link('product/category', $url)
+            );
+
+            $this->document->setTitle($this->language->get('text_error'));
+
+            $data['heading_title'] = $this->language->get('text_error');
+
+            $data['text_error'] = $this->language->get('text_error');
+
+            $data['button_continue'] = $this->language->get('button_continue');
+
+            $data['continue'] = $this->url->link('common/home');
+
+            $this->response->addHeader($this->request->server['SERVER_PROTOCOL'] . ' 404 Not Found');
+
+            $data['column_left'] = $this->load->controller('common/column_left');
+            $data['column_right'] = $this->load->controller('common/column_right');
+            $data['content_top'] = $this->load->controller('common/content_top');
+            $data['content_bottom'] = $this->load->controller('common/content_bottom');
+            $data['footer'] = $this->load->controller('common/footer');
+            $data['header'] = $this->load->controller('common/header');
+
+            if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/error/not_found.tpl')) {
+                $this->response->setOutput($this->load->view($this->config->get('config_template') . '/template/error/not_found.tpl', $data));
+            } else {
+                $this->response->setOutput($this->load->view('default/template/error/not_found.tpl', $data));
+            }
+        }
+	}
+}
---
+++ catalog/language/english/common/home.php
@@ -0,0 +1,8 @@
+<?php
+
+//Text
+$_['text_collection'] = 'New collection';
+$_['text_about']  = 'About Us';
+$_['text_store']   = 'Store';
+$_['text_oldcollection']   = 'Old collection';
+$_['text_logout']   = 'Only for registered user';
